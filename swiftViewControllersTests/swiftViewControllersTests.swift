15c0bd6980fd712d3de3682b1a88ce4cdba843f0
maciekmantis <maciej.scislowski@gmail.com>
2014-06-12 17:37:21 +0200

    commit

---
+++ swiftViewControllersTests/swiftViewControllersTests.swift
@@ -0,0 +1,35 @@
+//
+//  swiftViewControllersTests.swift
+//  swiftViewControllersTests
+//
+//  Created by Maciek on 12.06.2014.
+//  Copyright (c) 2014 Maciek. All rights reserved.
+//
+
+import XCTest
+
+class swiftViewControllersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
